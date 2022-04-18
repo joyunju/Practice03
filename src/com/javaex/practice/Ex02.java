75aea399be219a8e72f353ea869f2cfac525afd2
joyunju <whdbswn1113@naver.com>
2022-04-18 22:50:29 +0900

    for중첩문 - 별 프로그램

---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,16 @@
+//아래의 출력결과를 예상하여 작성하세요.
+
+package com.javaex.practice;
+
+public class Ex02 {
+	public static void main(String[] args) {
+
+		int x, y;
+		for (x = 0; x < 4; x++) {
+			for (y = 0; y < 2; y++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
