325a4186caef5ef8e161b113fbb4de411f8e955f
joyunju <whdbswn1113@naver.com>
2022-04-18 18:12:13 +0900

    while문과 for 반복문 비교

--- src/com/javaex/practice/Ex01.java
+++ src/com/javaex/practice/Ex01.java
@@ -3,5 +3,23 @@
 package com.javaex.practice;
 
 public class Ex01 {
+	public static void main(String[] args) {
 
+		System.out.println("==while문==");
+
+		int index = 0;
+		//조건식이 참이 될 때까지 실행
+		//while문과 for 반복문 비교 
+		
+		while (index < 3) {
+			System.out.println("index = " + index);
+			index++;
+		}
+
+		System.out.println("==for문==");
+
+		for (int order = 0; order < 3; order++) {
+			System.out.println("order = " + order);
+		}
+	}
 }
