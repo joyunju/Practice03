58557c4dfcb6b32e57d8f3f2af0d1035a2d608a4
joyunju <whdbswn1113@naver.com>
2022-04-27 03:24:55 +0900

    약수 구하기 - for - if

--- src/com/javaex/practice/Ex15.java
+++ src/com/javaex/practice/Ex15.java
@@ -1,9 +1,27 @@
+//Ex15 아래와 설명에 맞는 프로그램을 작성하세요
+//숫자를 1개 입력 받아 입력한 수의 약수를 모두 출력하세요
+
 package com.javaex.practice;
+
+import java.util.Scanner;
 
 public class Ex15 {
 
 	public static void main(String[] args) {
-		
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요 : ");
+
+		int num = sc.nextInt();
+
+		// 약수 구하기 : 나머지가 없이 딱 떨어지는 수
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0) {
+				System.out.println(i);
+			}
+		}
+
+		sc.close();
 
 	}
 
